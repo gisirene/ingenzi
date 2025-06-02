96f6acacb7b45ae1935ce19d6cced9d08a9b0242
Ange <ange.ingabire07@gmail.com>
2025-06-02 13:55:58 +0200

    Game-app created

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import gaming from './components/gamelist.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <gaming msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+  
+  </main>
+</template>
+
+<style scoped>
+.app {
+  max-width: 400px;
+  margin: auto;
+  text-align: center;
+  padding: 20px;
+  font-family: Arial;
+}
+
+input {
+  padding: 8px;
+  margin-right: 10px;
+  width: 60%;
+}
+
+button {
+  padding: 8px 12px;
+}
+
+ul {
+  margin-top: 20px;
+  text-align: left;
+}
+</style>
---
+++ src/components/gamelist.vue
@@ -0,0 +1,64 @@
+<script >
+  export default {
+    data() {
+      return {
+        welcomeMessage: '',
+        newGame: '',
+        gameList: []
+      };
+    },
+    methods: {
+      addGame() {
+        if (this.newGame.trim() !== '') {
+          this.gameList.push(this.newGame);
+          this.newGame = '';
+        }
+      }
+    },
+    mounted() {
+      this.welcomeMessage = 'Welcome to the Game Wishlist App!';
+    }
+  };
+</script>
+
+<template>
+  <div class="app">
+    <h1>{{ welcomeMessage }}</h1>
+
+    <input v-model="newGame" placeholder="Type a game title" />
+    <button @click="addGame">Add Game</button>
+
+    <h3>My Game Wishlist:</h3>
+    <ul>
+      <li v-for="(game, index) in gameList" :key="index">
+        {{ game }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  max-width: 400px;
+  margin: auto;
+  text-align: center;
+  padding: 20px;
+  font-family: Arial;
+}
+
+input {
+  padding: 8px;
+  margin-right: 10px;
+  width: 60%;
+}
+
+button {
+  padding: 8px 12px;
+}
+
+ul {
+  margin-top: 20px;
+  text-align: left;
+}
+
+</style>
